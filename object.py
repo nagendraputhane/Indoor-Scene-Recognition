e913df1a210a33456815de8a0e259b67655289b5
nagendra <nagendraputhane@gmail.com>
2020-04-16 14:08:42 +0530

    Sun DS but XML error

--- object.py
+++ object.py
@@ -10,7 +10,11 @@ class ContinueI(Exception): #utility function to skip to outer loop
 
 continue_i = ContinueI()
 
-fileList = glob.glob("C:\\Users\\NAGENDRA\\Desktop\\ISR\\*\\*.xml") #list of annotation files
+#list of annotation files
+#ISR
+#fileList = glob.glob("/home/internship_computervision/Nagendra/IndoorSceneRecognition/Annotations/*/*.xml")
+#SUN
+fileList = glob.glob("/home/internship_computervision/Nagendra/SUN2012pascalformat/Annotations/*.xml")
 
 #f = open("C:\\Users\\NAGENDRA\\Desktop\\files.csv", "w+")
 
@@ -30,28 +34,36 @@ for eachFile in fileList:
                     head, srcImgFile = path.split(src) #get file name
                     pre, ext = os.path.splitext(srcImgFile) #get extension
                     new_extension = '.jpg'
-                    os.rename(srcImgFile, pre + new_extension) #rename extention
-                    imgSrc = "C:\\JPEGImages\\" #folder that contains images
-                    imageFolderName = os.path.basename(os.path.dirname(src)) #the folder heirarchy for Indoor Scene Recognition database
-                    imgSrc = imgSrc + imageFolderName
-                    Path(imgSrc).mkdir(parents=True, exist_ok=True) #create folder
-                    imgSrc = imgSrc + srcImgFile #final images path
+                    imgSrc = "/home/internship_computervision/Nagendra/SUN2012pascalformat/JPEGImages/"
+                    imgSrc = imgSrc + pre + new_extension
+                    #os.rename(src + srcImgFile, imgSrc + pre + new_extension) #rename extention
+                    #imgSrc = "/home/internship_computervision/Nagendra/IndoorSceneRecognition/Images/" #folder that contains images
+                    #imgSrc = "/home/internship_computervision/Nagendra/SUN2012pascalformat/JPEGImages/" #folder that contains images
+                    #imageFolderName = os.path.basename(os.path.dirname(src)) #the folder heirarchy for Indoor Scene Recognition database
+                    #imgSrc = imgSrc + imageFolderName
+                    #Path(imgSrc).mkdir(parents=True, exist_ok=True) #create folder
+                    #imgSrc = imgSrc + srcImgFile #final images path
 
                     folderName = os.path.basename(os.path.dirname(src))
                     head, tail = path.split(src)
-                    dst = "C:\\Users\\NAGENDRA\\Desktop\\new_b\\" + folderName + "\\" #new location for annotations
-                    Path(dst).mkdir(parents=True, exist_ok=True)
+                    dst = "/home/internship_computervision/Nagendra/PeopleIndoor/SUN/Annotations/"
+                    #dst = "/home/internship_computervision/Nagendra/PeopleIndoor/ISR/Annotations/" + folderName + "/" #new location for annotations
+                    #Path(dst).mkdir(parents=True, exist_ok=True)
                     dst = dst + tail #final annotations destination path
 
                     head, dstImgFile = path.split(dst)
                     pre, ext = os.path.splitext(dstImgFile)
                     new_extension = '.jpg'
-                    os.rename(dstImgFile, pre + new_extension)
-                    imgDst = "C:\\JPEGImages\\" #new location for images
-                    imageFolderName = os.path.basename(os.path.dirname(src))
-                    imgDst = imgDst + imageFolderName
-                    Path(imgDst).mkdir(parents=True, exist_ok=True)
-                    imgDst = imgDst + dstImgFile #final images destination path
+                    imgDst = "/home/internship_computervision/Nagendra/PeopleIndoor/SUN/Images/"
+                    imgDst = imgDst + pre + new_extension
+                    #os.rename(dst + dstImgFile, imgDst + pre + new_extension)
+                    #imgDst = "/home/internship_computervision/Nagendra/PeopleIndoor/SUN/Images/" #new location for images
+                    #imgDst = "/home/internship_computervision/Nagendra/PeopleIndoor/ISR/Images/" #new location for images
+                    #imageFolderName = os.path.basename(os.path.dirname(src))
+                    #imgDst = imgDst + imageFolderName
+                    #Path(imgDst).mkdir(parents=True, exist_ok=True)
+                    #imgDst = imgDst + dstImgFile #final images destination path
+                    #Path(imgDst).mkdir(parents=True, exist_ok=True)
 
                     shutil.copy(src, dst) #copy annotations
                     shutil.copy(imgSrc, imgDst) #copy images
